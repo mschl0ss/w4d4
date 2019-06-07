cfa721bf9940f22da1ac0ac44197adfd0498dbc4
Marc Schlossberg <mr.schlossberg@gmail.com>
2019-06-06 17:31:32 -0700

    Add new band album functionality.  Albums list under their appropriate band in the band list.

--- w4d4/Muzak/app/controllers/albums_controller.rb
+++ w4d4/Muzak/app/controllers/albums_controller.rb
@@ -7,7 +7,29 @@ class AlbumsController < ApplicationController
     end
 
     def new
+        @band = Band.find(params[:band_id])
+        @bands = Band.all
         @album = Album.new
-        
+
+        render :new
+
+    end
+
+    def create
+        @album = Album.new(album_params)
+        # debugger
+        if @album.save!
+            redirect_to bands_url
+        else
+            flash.now[:errors] = ['bad album.  I mean possibly a good album, but you did it wrong.']
+            render :new
+        end
+
+    end
+
+
+    private
+    def album_params
+        params.require(:album).permit(:title,:year,:band_id)
     end
 end
